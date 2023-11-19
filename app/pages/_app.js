74b2509e7ebfa1f817b4ac58e12b0ceba426d3a4
sam-gvt <compteprosam@gmail.com>
2023-11-19 15:36:07 +1100

    first commit

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,8 +1,7 @@
 import { useMemo } from "react";
 import { clusterApiUrl } from "@solana/web3.js";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { WalletModalProvider,  WalletMultiButton, WalletDisconnectButton } from "@solana/wallet-adapter-react-ui";
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 
 import "../styles/App.css";
@@ -13,7 +12,8 @@ import "@solana/wallet-adapter-react-ui/styles.css";
 const App = ({ Component, pageProps }) => {
     const network = WalletAdapterNetwork.Devnet;
     const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-    const wallets = useMemo(() => [new PhantomWalletAdapter()], [network]);
+    const wallets = useMemo(() => [], [network]);
+
 
     return (
         <ConnectionProvider endpoint={endpoint}>
--- app/pages/index.js
+++ app/pages/index.js
@@ -1,24 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // Constants
 const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const Home = () => {
-    return (
-        <div className="App">
-            <div className="container">
-                <div className="header-container">
-                    <p className="header">🍭 Candy Drop</p>
-                    <p className="sub-text">NFT drop machine with fair mint</p>
-                </div>
-                <div className="footer-container">
-                    <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-                    <a className="footer-text" href={TWITTER_LINK} target="_blank" rel="noreferrer">{`built on @${TWITTER_HANDLE}`}</a>
-                </div>
-            </div>
+
+
+  const [walletAddress, setWalletAddress] = useState(null)
+
+  const checkIfWalletIsConnected = async () => {
+      console.log("objet window : ", window.solana);
+      try {
+        const { solana } = window;
+        if (solana) {
+          if (solana.isPhantom) {
+            console.log("Phantom wallet found !");
+            const response = await solana.connect({onlyIfTrusted: true})
+            console.log('Connect with Public key: ', response.publicKey.toString());
+
+            setWalletAddress(response.publicKey.toString())
+          }
+        } else {
+          alert("Solana object not found, please get a Phantom Wallet !");
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    
+  };
+
+  const connectWallet = async() => {
+    const {solana} = window;
+    if (solana) {
+        const response = await solana.connect()
+        console.log('Connect with Public key: ', response.publicKey.toString());
+        setWalletAddress(response.publicKey.toString())
+
+    }
+  }
+
+  const renderNotConnectedContainer = () => (
+    <button className="cta-button connect-wallet-button" onClick={connectWallet}>Connect</button>
+  )
+
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    //'load' se produit lorsque la page web et
+    // tous ses éléments (images, feuilles de style, etc.) ont été complètement chargés.
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header">🍭 Candy Drop</p>
+          <p className="sub-text">NFT drop machine with fair mint</p>
+          { !walletAddress && renderNotConnectedContainer()}
         </div>
-    );
+        <div className="footer-container">
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built on @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
